2005bc5ba07cffa65d27fee5ed190cb30a3340f5
Menno <mennobil02@gmail.com>
2023-12-09 13:09:32 +0100

    [CHANGED] Some comments in renderer idea file

--- Core/src/RabBit/graphics/Renderer.h
+++ Core/src/RabBit/graphics/Renderer.h
@@ -18,13 +18,23 @@ namespace RB::Graphics
 		uint32_t customHeight;
 	};
 
+	enum class TextureFlags : uint32_t
+	{
+		None						= (1 << 0);
+		ClearAtFrameStart			= (1 << 1);
+		ClearAtRenderContextStart	= (1 << 2);
+		ClearAtRenderPassStart		= (1 << 3);
+		Keep1FrameHistory			= (1 << 4);
+	};
+
 	struct RenderTextureDesc
 	{
 		char* name;
-		bool needsHistory; // If this is true, maybe then create 2 of these textures and ping pong between the two when giving it to the pass, so it can also read 1 frame back
 		void* format;
 		TextureSize size;
 		uint32_t flags;
+
+		// Maybe also add a fence value that indicates that this texture should wait on that fence value before being used by another commandlist again
 	};
 
 
@@ -59,20 +69,57 @@ namespace RB::Graphics
 
 	class RenderPass
 	{
-		static 
 		static RenderPassConfig GetConfiguration();
 
 		// The return value is the output of this pass
 		RenderTexture* Render(CommandList* commandList, RenderTexture** workingTextures, RenderPass** dependencies)
 	};
 
+	class RenderRect
+	{
+		float top;
+		float bottom;
+		float left;
+		float right;
+		float width;
+		float height;
+		float aspectRatio;
+		float aspectRatioFull;
+	};
+
+	class Float4x4;
+
+	class RenderContext
+	{
+		RenderPass* passes;	// A rendercontext should also be able to have overwrites (offsets of setting enums) of each RenderPass
+		Float4x4 viewProjection;
+		RenderRect renderRect; // Render location
+		RenderTexture colorOutput;
+		RenderTexture uiOutput;
+
+		void Render()
+		{
+			// Render all passes
+		}
+	};
+
 	class Renderer
 	{
-		void RenderFrame()
+		RenderContext* contexts;
+
+		void RenderContexts()
 		{
-			// First do render passes. These passes should not render to the swapchain backbuffer but use a "game backbuffer" which can have a different resolution
-			// than the swapchain backbuffer. After the render passes, there will then be a composite pass, or finalize pass, which will "down-/upscale" it to the 
-			// swapchain backbuffer resolution using a sampler in the pixel shader and merge the UI with the color texture (as the color and UI textures should be separate).
+			/*
+				First render the different render contexts.
+				After each context then something with the color and ui outputs has to be done, but what?
+					- Merge the color and UI together and use as input for next render context (probably worst solution)
+					- Copy the UI with the UI of the next render context
+					- Maybe put in the renderpassconfig a thing to be able to receive optional previous contexts UI and/or color outputs as input
+
+				After all contexts are rendered, first upscale the final colorOutput with the chosen upscaler, if any selected. After that, combine the upscaled color and the UI output into the "game backbuffer".
+				This "game backbuffer" can have a different resolution than the swapchain backbuffer (if the user selects 1440p rendering with a 4k display while playing in fullscreen). After the merge,
+				add a finalize path which "upscales/downscales", with a sampler in the pixel shader, to the actual swapchain backbuffer.
+			*/
 		}
 	};
 }
